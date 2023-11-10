f016f0461b19d84d8a57c1449e89ef4939d99eea
root <root@ip-172-31-40-63.ap-southeast-1.compute.internal>
2023-11-10 12:59:06 +0000

    adding tested data

--- app/apigateway.tf
+++ app/apigateway.tf
@@ -76,19 +76,14 @@ resource "aws_api_gateway_deployment" "example" {
 }
 
 
-
-
-resource "aws_api_gateway_resource" "MyDemoResource" {
-  rest_api_id = aws_api_gateway_rest_api.example.id
-  parent_id   = aws_api_gateway_rest_api.example.root_resource_id
-  path_part   = "mydemoresource"
-}
-
 resource "aws_api_gateway_method" "MyDemoMethod" {
   rest_api_id   = aws_api_gateway_rest_api.example.id
-  resource_id   = aws_api_gateway_resource.example.id
-  http_method   = "GET"
+  resource_id   = aws_api_gateway_resource.ApiProxyResource.id
+  http_method   = "ANY"
   authorization = "NONE"
+  request_parameters = {
+    "method.request.path.proxy" = true
+  }
 }
 
 
@@ -98,7 +93,31 @@ resource "aws_api_gateway_resource" "ApiProxyResource" {
   path_part   = "{proxy+}"
 }
 
+resource "aws_api_gateway_integration" "proxyexample" {
+  rest_api_id = aws_api_gateway_rest_api.example.id
+  resource_id = aws_api_gateway_resource.ApiProxyResource.id
+  http_method = aws_api_gateway_method.MyDemoMethod.http_method
+  integration_http_method = "POST"  # Specify the HTTP method of your Lambda function
+  type       = "AWS_PROXY"
+  uri        = aws_lambda_function.this.invoke_arn
+}
 
+resource "aws_api_gateway_method_response" "proxy_example" {
+  rest_api_id = aws_api_gateway_rest_api.example.id
+  resource_id = aws_api_gateway_resource.ApiProxyResource.id
+  http_method = aws_api_gateway_method.MyDemoMethod.http_method
+  status_code = "200"
+#  response_models = {
+#    "application/json" = "Empty"  # You can specify a different response model if needed
+#  }
+}
+
+
+resource "aws_api_gateway_deployment" "proxy_example" {
+  depends_on = [aws_api_gateway_integration.proxyexample]
+  rest_api_id = aws_api_gateway_rest_api.example.id
+  stage_name = "dev"  # Specify the desired stage name
+}
 
 # Apigw key and usage plan
 
